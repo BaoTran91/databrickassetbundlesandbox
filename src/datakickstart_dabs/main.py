280f48c229bbe4bc2ef030aa15abf1c61684c39c
Bao Tran <baotran@Baos-MBP.attlocal.net>
2025-10-03 14:42:01 -0400

    updated

--- src/datakickstart_dabs/main.py
+++ src/datakickstart_dabs/main.py
@@ -2,7 +2,12 @@ from databricks.sdk.runtime import spark
 from pyspark.sql import DataFrame
 
 def find_all_taxis() -> DataFrame:
-    return spark.read.table("samples.nyctaxi.trips")
+    return spark.sql("""
+    SELECT artist_name, total_number_of_songs, year
+    FROM sandbox.tutorial.top_artists_by_year
+    WHERE year >= 1990
+    ORDER BY total_number_of_songs DESC, year DESC;
+    """)
 
 def main():
     find_all_taxis().show(5)
